ece6121ce8075b25595fa51a002d5f94cf317699
GOROGODO TAKUNDA LEONARD <gorogodotakundah26@gmail.com>
2026-05-30 00:36:45 +0530

    Add files via upload

---
+++ MiniProjects/answers.py
@@ -0,0 +1,7 @@
+options = [
+    ["A. Inheritance", "B. Abstraction", "C. Polymorphism", "D. Objects"],
+    ["A. Single", "B. Double", "C. Multiple", "D. both A and C"],
+    ["A. Multiple Inheritance", "B. Multilevel Inheritance", "C. Hierarchical Inheritance", "D. None of these"],
+    ["A. Two level", "B. Three level", "C. Any level", "D. None of these"],
+    ["A. Method Recursive Object", "B. Method Resolution Order", "C. Main Resolution Order", "D. Method Resolution Object"]
+]
